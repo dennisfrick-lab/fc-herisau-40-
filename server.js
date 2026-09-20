6a59cbf115fee2f45684536b2e20e917ab4d571c
dennisfrick-lab <dennisfrick@hotmail.ch>
2026-09-21 00:17:33 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+const PUBLIC = path.join(__dirname, "public");
+
+const types = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".js": "text/javascript; charset=utf-8",
+  ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".svg": "image/svg+xml"
+};
+
+http.createServer((req, res) => {
+  let url = req.url.split("?")[0];
+  if (url === "/") url = "/index.html";
+
+  const file = path.join(PUBLIC, url);
+
+  if (!file.startsWith(PUBLIC)) {
+    res.writeHead(403);
+    return res.end("Forbidden");
+  }
+
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      return res.end("Not found");
+    }
+
+    res.writeHead(200, {
+      "Content-Type": types[path.extname(file)] || "application/octet-stream"
+    });
+    res.end(data);
+  });
+}).listen(PORT, "0.0.0.0", () => {
+  console.log(`FC Herisau 40+ läuft auf Port ${PORT}`);
+});
